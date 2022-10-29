e6730f1f9f3c7f398a71c3e993c31b19a547a879
npax1 <prakh55555@gmail.com>
2022-10-29 10:12:14 +0300

    done work

--- venv/work1.py
+++ venv/work1.py
@@ -1,14 +1,14 @@
 
-try:
-    def truk(a,b):
+def truk(a,b):
+    try:
         for i in range (0, a):
             for j in range(0, b):
-                print('*',end= "")
+                print('*',end= " ")
             print()
         print()
-    print(truk)
 
-except Exception as ex:
-    print(f"error information:{ex}")
-finally:
-    print("exit")
+    except Exception as ex:
+        print(f"error information:{ex}")
+
+
+truk(2, 7)
