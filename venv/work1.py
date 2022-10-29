447fdff8b36f1996740b2a3e3c274ccab58fafa7
npax1 <prakh55555@gmail.com>
2022-10-29 11:26:14 +0300

    not  done work

--- venv/work1.py
+++ venv/work1.py
@@ -1,14 +1,1 @@
-
-
-def bigger(a, b):
-    if a>b:
-        return a
-    elif a<b:
-        return b
-    else:
-        return false
-
-
-a = int(input("Enter a number: "))
-b = int(input("Enter a number2: "))
-print(bigger(a, b))
+def per
